aeb5110d042608850b0ed79ea0e91d2e33cde7d4
Dan Lynch <pyramation@gmail.com>
2020-11-23 22:10:29 -0700

    upgrade totp

--- packages/totp/deploy/schemas/totp/procedures/generate_totp.sql
+++ packages/totp/deploy/schemas/totp/procedures/generate_totp.sql
@@ -9,206 +9,6 @@ BEGIN;
 -- http://blog.tinisles.com/2011/10/google-authenticator-one-time-password-algorithm-in-javascript/
 -- https://gist.github.com/bwbroersma/676d0de32263ed554584ab132434ebd9
 
-CREATE FUNCTION totp.t_unix (
-  timestamptz
-)
-  RETURNS bigint
-  AS $$
-SELECT floor(EXTRACT(epoch FROM $1))::bigint;
-$$
-LANGUAGE sql IMMUTABLE;
-
-CREATE FUNCTION totp.n (
-  t timestamptz,
-  step bigint default 30
-)
-  RETURNS bigint
-  AS $$
-SELECT floor(totp.t_unix(t) / step)::bigint;
-$$
-LANGUAGE sql IMMUTABLE;
-
-CREATE FUNCTION totp.n_hex (
-  n bigint
-)
-  RETURNS text
-  AS $$
-DECLARE
- missing_padding int;
- hext text;
-BEGIN
-  hext = to_hex(n);
-  RETURN lpad(hext, 16, '0');
-END;
-$$
-LANGUAGE plpgsql
-IMMUTABLE;
-
-CREATE FUNCTION totp.generate_totp_time_key (
-  totp_interval int DEFAULT 30,
-  from_time timestamptz DEFAULT NOW()
-)
-  RETURNS text
-  AS $$
-  SELECT totp.n_hex( totp.n ( from_time, totp_interval ) );
-$$
-LANGUAGE 'sql'
-IMMUTABLE;
-
- -- '0000000003114810' -> '\x0000000003114810'::bytea
-CREATE FUNCTION totp.n_hex_to_8_bytes(
-  input text
-) returns bytea as $$
-DECLARE
-  b bytea;
-  buf text;
-BEGIN
-    buf = 'SELECT ''\x' || input || '''::bytea';
-    EXECUTE buf INTO b;
-    RETURN b;
-END;
-$$
-LANGUAGE 'plpgsql' IMMUTABLE;
-
-CREATE FUNCTION totp.hmac_as_20_bytes(
-  n_hex bytea,
-  v_secret bytea,
-  v_algo text default 'sha1'
-) returns bytea as $$
-DECLARE
-  v_hmac bytea;
-BEGIN
-  RETURN hmac(n_hex, v_secret, v_algo);
-END;
-$$
-LANGUAGE 'plpgsql' IMMUTABLE;
-
-CREATE FUNCTION totp.get_offset (
-  hmac_as_20_bytes bytea
-) returns int as $$
-DECLARE
-  v_hmac bytea;
-  v_str text;
-  buf text;
-  ch text;
-  i int;
-BEGIN
-
-    -- get last char (or last 4 bits as int)
-    ch = right(hmac_as_20_bytes::text, 1); 
-    buf = 'SELECT x''' || ch || '''::int';
-    EXECUTE buf INTO i;
-    RETURN i;
-
-    -- TEST BELOW FOR MORE CASES for now I'm doing the simpler version
-
-    -- 160 bits in 20 bytes... so get last 4 bits:
-    -- you may wonder why these numbers?
-    -- e.g., 0x9A => A => 0b1010 => 10 (int)
-    -- it's not x x x x 1 0 1 0 ...
-    -- it's actually 
-        --  A                   9
-    -- [0] [1] [0] [1] [ ] [ ] [ ] [ ] 
-    v_str = concat(
-      '0000',
-      get_bit( hmac_as_20_bytes, 155),
-      get_bit( hmac_as_20_bytes, 154),
-      get_bit( hmac_as_20_bytes, 153),
-      get_bit( hmac_as_20_bytes, 152)
-    );
-
-    buf = 'SELECT B''' || v_str || '''::int';
-    EXECUTE buf INTO i;
-
-    RETURN i;
-
-END;
-$$
-LANGUAGE 'plpgsql' IMMUTABLE;
-
-
-
-CREATE FUNCTION totp.get_first_4_bytes_from_offset (
-  hmac_as_20_bytes bytea,
-  v_offset int
-) returns int[] as $$
-DECLARE
-  a int;
-  b int;
-  c int;
-  d int;
-BEGIN
-
-  a = get_byte(hmac_as_20_bytes, v_offset);
-  b = get_byte(hmac_as_20_bytes, v_offset + 1);
-  c = get_byte(hmac_as_20_bytes, v_offset + 2);
-  d = get_byte(hmac_as_20_bytes, v_offset + 3);
-
-  RETURN ARRAY[a,b,c,d]::int[];
-
-END;
-$$
-LANGUAGE 'plpgsql' IMMUTABLE;
-
-CREATE FUNCTION totp.apply_binary_to_bytes (
-  four_bytes int[]
-) returns int[] as $$
-BEGIN
-  four_bytes[1] = four_bytes[1] & 127; -- x'7f';
-  four_bytes[2] = four_bytes[2] & 255; -- x'ff';
-  four_bytes[3] = four_bytes[3] & 255; -- x'ff';
-  four_bytes[4] = four_bytes[4] & 255; -- x'ff';
-
-  RETURN four_bytes;
-
-END;
-$$
-LANGUAGE 'plpgsql' IMMUTABLE;
-
-CREATE FUNCTION totp.compact_bytes_to_int (
-  four_bytes int[]
-) returns int as $$
-DECLARE 
-  buf text;
-  i int;
-BEGIN
-  buf = 
-   to_hex(four_bytes[1]) ||
-   to_hex(four_bytes[2]) ||
-   to_hex(four_bytes[3]) ||
-   to_hex(four_bytes[4]);
-
-  buf = 'SELECT x''' || buf || '''::int';
-  EXECUTE buf INTO i;
-  RETURN i;
-END;
-$$
-LANGUAGE 'plpgsql' IMMUTABLE;
-
-CREATE FUNCTION totp.calculate_token (
-  calcd_int int,
-  totp_length int default 6
-) returns text as $$
-DECLARE 
-  buf text;
-  i int;
-  s text;
-  missing_padding int;
-BEGIN
-   i = calcd_int % (10^totp_length)::int;
-   s = i::text;
-
-   -- if token size < totp_len, padd with zeros
-   missing_padding = character_length(s) % totp_length;
-   if missing_padding != 0 THEN
-     s = lpad('', (totp_length - missing_padding), '0') || s;
-   END IF;
-
-   RETURN s;
-END;
-$$
-LANGUAGE 'plpgsql' IMMUTABLE;
-
 CREATE FUNCTION totp.pad_secret (
   input bytea,
   len int
@@ -257,65 +57,6 @@ END;
 $$
 LANGUAGE 'plpgsql' IMMUTABLE;
 
-CREATE FUNCTION totp.generate_procedural (
-  totp_secret text,
-  totp_interval int default 30,
-  totp_length int default 6,
-  time_from timestamptz DEFAULT NOW(),
-  algo text default 'sha1',
-  encoding text default NULL
-) returns text as $$
-DECLARE 
-  v_bytes_int int;
-  n int;
-  v_hmc bytea;
-  v_offset int;
-
-  v_secret bytea;
-BEGIN
-  n = totp.n(
-    time_from,
-    totp_interval
-  );
-
-  IF (encoding = 'base32') THEN 
-    v_secret = ( '\x' || totp.base32_to_hex(totp_secret) )::bytea;
-  ELSE 
-    v_secret = totp_secret::bytea;
-  END IF;
-
-  -- v_secret = totp.pad_secret(v_secret, 20);
-
-  v_hmc = totp.hmac_as_20_bytes( 
-    totp.n_hex_to_8_bytes(
-      totp.n_hex(n)
-    ),
-    v_secret,
-    algo
-  );
-
-  v_offset = totp.get_offset(
-    v_hmc
-  );
-
-  v_bytes_int = totp.compact_bytes_to_int( 
-    totp.apply_binary_to_bytes(
-      totp.get_first_4_bytes_from_offset(
-        v_hmc,
-        v_offset
-      )
-    )
-  );
-
-  RETURN totp.calculate_token(
-    v_bytes_int,
-    totp_length
-  );
-
-END;
-$$
-LANGUAGE 'plpgsql' VOLATILE;
-
 CREATE FUNCTION totp.hotp(key BYTEA, c INT, digits INT DEFAULT 6, hash TEXT DEFAULT 'sha1') RETURNS TEXT AS $$
 DECLARE
     c BYTEA := '\x' || LPAD(TO_HEX(c), 16, '0');
@@ -330,14 +71,14 @@ $$ LANGUAGE plpgsql IMMUTABLE;
 CREATE FUNCTION totp.generate(
     secret text, 
     period int DEFAULT 30,
-    digits INT DEFAULT 6, 
+    digits int DEFAULT 6, 
     time_from timestamptz DEFAULT NOW(),
-    hash TEXT DEFAULT 'sha1',
-    encoding TEXT DEFAULT 'base32',
-    clock_offset INT DEFAULT 0
-) RETURNS TEXT AS $$
+    hash text DEFAULT 'sha1',
+    encoding text DEFAULT 'base32',
+    clock_offset int DEFAULT 0
+) RETURNS text AS $$
 DECLARE
-    c INT := FLOOR(EXTRACT(EPOCH FROM time_from) / period)::INT + clock_offset;
+    c int := FLOOR(EXTRACT(EPOCH FROM time_from) / period)::int + clock_offset;
     key bytea;
 BEGIN
 
@@ -351,21 +92,6 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql STABLE;
 
-CREATE FUNCTION totp.generate_secret(hash TEXT DEFAULT 'sha1') RETURNS BYTEA AS $$
-BEGIN
-    -- See https://tools.ietf.org/html/rfc4868#section-2.1.2
-    -- The optimal key length for HMAC is the block size of the algorithm
-    CASE
-          WHEN hash = 'sha1'   THEN RETURN gen_random_bytes(20); -- = 160 bits
-          WHEN hash = 'sha256' THEN RETURN gen_random_bytes(32); -- = 256 bits
-          WHEN hash = 'sha512' THEN RETURN gen_random_bytes(64); -- = 512 bits
-          ELSE
-            RAISE EXCEPTION 'Unsupported hash algorithm for OTP (see RFC6238/4226).';
-            RETURN NULL;
-    END CASE;
-END;
-$$ LANGUAGE plpgsql VOLATILE;
-
 CREATE FUNCTION totp.verify (
   secret text,
   check_totp text,
@@ -373,7 +99,7 @@ CREATE FUNCTION totp.verify (
   digits int default 6,
   time_from timestamptz DEFAULT NOW(),
   hash text default 'sha1',
-  encoding text default NULL,
+  encoding text DEFAULT 'base32',
   clock_offset int default 0
 )
   RETURNS boolean
--- packages/totp/sql/launchql-totp--0.0.1.sql
+++ packages/totp/sql/launchql-totp--0.0.1.sql
@@ -32,154 +32,6 @@ BEGIN
 END;
 $EOFCODE$ LANGUAGE plpgsql STRICT IMMUTABLE;
 
-CREATE FUNCTION totp.t_unix (  timestamptz ) RETURNS bigint AS $EOFCODE$
-SELECT floor(EXTRACT(epoch FROM $1))::bigint;
-$EOFCODE$ LANGUAGE sql IMMUTABLE;
-
-CREATE FUNCTION totp.n ( t timestamptz, step bigint DEFAULT 30 ) RETURNS bigint AS $EOFCODE$
-SELECT floor(totp.t_unix(t) / step)::bigint;
-$EOFCODE$ LANGUAGE sql IMMUTABLE;
-
-CREATE FUNCTION totp.n_hex ( n bigint ) RETURNS text AS $EOFCODE$
-DECLARE
- missing_padding int;
- hext text;
-BEGIN
-  hext = to_hex(n);
-  RETURN lpad(hext, 16, '0');
-END;
-$EOFCODE$ LANGUAGE plpgsql IMMUTABLE;
-
-CREATE FUNCTION totp.generate_totp_time_key ( totp_interval int DEFAULT 30, from_time timestamptz DEFAULT now() ) RETURNS text AS $EOFCODE$
-  SELECT totp.n_hex( totp.n ( from_time, totp_interval ) );
-$EOFCODE$ LANGUAGE sql IMMUTABLE;
-
-CREATE FUNCTION totp.n_hex_to_8_bytes ( input text ) RETURNS bytea AS $EOFCODE$
-DECLARE
-  b bytea;
-  buf text;
-BEGIN
-    buf = 'SELECT ''\x' || input || '''::bytea';
-    EXECUTE buf INTO b;
-    RETURN b;
-END;
-$EOFCODE$ LANGUAGE plpgsql IMMUTABLE;
-
-CREATE FUNCTION totp.hmac_as_20_bytes ( n_hex bytea, v_secret bytea, v_algo text DEFAULT 'sha1' ) RETURNS bytea AS $EOFCODE$
-DECLARE
-  v_hmac bytea;
-BEGIN
-  RETURN hmac(n_hex, v_secret, v_algo);
-END;
-$EOFCODE$ LANGUAGE plpgsql IMMUTABLE;
-
-CREATE FUNCTION totp.get_offset ( hmac_as_20_bytes bytea ) RETURNS int AS $EOFCODE$
-DECLARE
-  v_hmac bytea;
-  v_str text;
-  buf text;
-  ch text;
-  i int;
-BEGIN
-
-    -- get last char (or last 4 bits as int)
-    ch = right(hmac_as_20_bytes::text, 1); 
-    buf = 'SELECT x''' || ch || '''::int';
-    EXECUTE buf INTO i;
-    RETURN i;
-
-    -- TEST BELOW FOR MORE CASES for now I'm doing the simpler version
-
-    -- 160 bits in 20 bytes... so get last 4 bits:
-    -- you may wonder why these numbers?
-    -- e.g., 0x9A => A => 0b1010 => 10 (int)
-    -- it's not x x x x 1 0 1 0 ...
-    -- it's actually 
-        --  A                   9
-    -- [0] [1] [0] [1] [ ] [ ] [ ] [ ] 
-    v_str = concat(
-      '0000',
-      get_bit( hmac_as_20_bytes, 155),
-      get_bit( hmac_as_20_bytes, 154),
-      get_bit( hmac_as_20_bytes, 153),
-      get_bit( hmac_as_20_bytes, 152)
-    );
-
-    buf = 'SELECT B''' || v_str || '''::int';
-    EXECUTE buf INTO i;
-
-    RETURN i;
-
-END;
-$EOFCODE$ LANGUAGE plpgsql IMMUTABLE;
-
-CREATE FUNCTION totp.get_first_4_bytes_from_offset ( hmac_as_20_bytes bytea, v_offset int ) RETURNS int[] AS $EOFCODE$
-DECLARE
-  a int;
-  b int;
-  c int;
-  d int;
-BEGIN
-
-  a = get_byte(hmac_as_20_bytes, v_offset);
-  b = get_byte(hmac_as_20_bytes, v_offset + 1);
-  c = get_byte(hmac_as_20_bytes, v_offset + 2);
-  d = get_byte(hmac_as_20_bytes, v_offset + 3);
-
-  RETURN ARRAY[a,b,c,d]::int[];
-
-END;
-$EOFCODE$ LANGUAGE plpgsql IMMUTABLE;
-
-CREATE FUNCTION totp.apply_binary_to_bytes ( four_bytes int[] ) RETURNS int[] AS $EOFCODE$
-BEGIN
-  four_bytes[1] = four_bytes[1] & 127; -- x'7f';
-  four_bytes[2] = four_bytes[2] & 255; -- x'ff';
-  four_bytes[3] = four_bytes[3] & 255; -- x'ff';
-  four_bytes[4] = four_bytes[4] & 255; -- x'ff';
-
-  RETURN four_bytes;
-
-END;
-$EOFCODE$ LANGUAGE plpgsql IMMUTABLE;
-
-CREATE FUNCTION totp.compact_bytes_to_int ( four_bytes int[] ) RETURNS int AS $EOFCODE$
-DECLARE 
-  buf text;
-  i int;
-BEGIN
-  buf = 
-   to_hex(four_bytes[1]) ||
-   to_hex(four_bytes[2]) ||
-   to_hex(four_bytes[3]) ||
-   to_hex(four_bytes[4]);
-
-  buf = 'SELECT x''' || buf || '''::int';
-  EXECUTE buf INTO i;
-  RETURN i;
-END;
-$EOFCODE$ LANGUAGE plpgsql IMMUTABLE;
-
-CREATE FUNCTION totp.calculate_token ( calcd_int int, totp_length int DEFAULT 6 ) RETURNS text AS $EOFCODE$
-DECLARE 
-  buf text;
-  i int;
-  s text;
-  missing_padding int;
-BEGIN
-   i = calcd_int % (10^totp_length)::int;
-   s = i::text;
-
-   -- if token size < totp_len, padd with zeros
-   missing_padding = character_length(s) % totp_length;
-   if missing_padding != 0 THEN
-     s = lpad('', (totp_length - missing_padding), '0') || s;
-   END IF;
-
-   RETURN s;
-END;
-$EOFCODE$ LANGUAGE plpgsql IMMUTABLE;
-
 CREATE FUNCTION totp.pad_secret ( input bytea, len int ) RETURNS bytea AS $EOFCODE$
 DECLARE 
   output bytea;
@@ -221,57 +73,6 @@ BEGIN
 END;
 $EOFCODE$ LANGUAGE plpgsql IMMUTABLE;
 
-CREATE FUNCTION totp.generate_procedural ( totp_secret text, totp_interval int DEFAULT 30, totp_length int DEFAULT 6, time_from timestamptz DEFAULT now(), algo text DEFAULT 'sha1', encoding text DEFAULT NULL ) RETURNS text AS $EOFCODE$
-DECLARE 
-  v_bytes_int int;
-  n int;
-  v_hmc bytea;
-  v_offset int;
-
-  v_secret bytea;
-BEGIN
-  n = totp.n(
-    time_from,
-    totp_interval
-  );
-
-  IF (encoding = 'base32') THEN 
-    v_secret = ( '\x' || totp.base32_to_hex(totp_secret) )::bytea;
-  ELSE 
-    v_secret = totp_secret::bytea;
-  END IF;
-
-  -- v_secret = totp.pad_secret(v_secret, 20);
-
-  v_hmc = totp.hmac_as_20_bytes( 
-    totp.n_hex_to_8_bytes(
-      totp.n_hex(n)
-    ),
-    v_secret,
-    algo
-  );
-
-  v_offset = totp.get_offset(
-    v_hmc
-  );
-
-  v_bytes_int = totp.compact_bytes_to_int( 
-    totp.apply_binary_to_bytes(
-      totp.get_first_4_bytes_from_offset(
-        v_hmc,
-        v_offset
-      )
-    )
-  );
-
-  RETURN totp.calculate_token(
-    v_bytes_int,
-    totp_length
-  );
-
-END;
-$EOFCODE$ LANGUAGE plpgsql VOLATILE;
-
 CREATE FUNCTION totp.hotp ( key bytea, c int, digits int DEFAULT 6, hash text DEFAULT 'sha1' ) RETURNS text AS $EOFCODE$
 DECLARE
     c BYTEA := '\x' || LPAD(TO_HEX(c), 16, '0');
@@ -285,7 +86,7 @@ $EOFCODE$ LANGUAGE plpgsql IMMUTABLE;
 
 CREATE FUNCTION totp.generate ( secret text, period int DEFAULT 30, digits int DEFAULT 6, time_from timestamptz DEFAULT now(), hash text DEFAULT 'sha1', encoding text DEFAULT 'base32', clock_offset int DEFAULT 0 ) RETURNS text AS $EOFCODE$
 DECLARE
-    c INT := FLOOR(EXTRACT(EPOCH FROM time_from) / period)::INT + clock_offset;
+    c int := FLOOR(EXTRACT(EPOCH FROM time_from) / period)::int + clock_offset;
     key bytea;
 BEGIN
 
@@ -299,22 +100,7 @@ BEGIN
 END;
 $EOFCODE$ LANGUAGE plpgsql STABLE;
 
-CREATE FUNCTION totp.generate_secret ( hash text DEFAULT 'sha1' ) RETURNS bytea AS $EOFCODE$
-BEGIN
-    -- See https://tools.ietf.org/html/rfc4868#section-2.1.2
-    -- The optimal key length for HMAC is the block size of the algorithm
-    CASE
-          WHEN hash = 'sha1'   THEN RETURN gen_random_bytes(20); -- = 160 bits
-          WHEN hash = 'sha256' THEN RETURN gen_random_bytes(32); -- = 256 bits
-          WHEN hash = 'sha512' THEN RETURN gen_random_bytes(64); -- = 512 bits
-          ELSE
-            RAISE EXCEPTION 'Unsupported hash algorithm for OTP (see RFC6238/4226).';
-            RETURN NULL;
-    END CASE;
-END;
-$EOFCODE$ LANGUAGE plpgsql VOLATILE;
-
-CREATE FUNCTION totp.verify ( secret text, check_totp text, period int DEFAULT 30, digits int DEFAULT 6, time_from timestamptz DEFAULT now(), hash text DEFAULT 'sha1', encoding text DEFAULT NULL, clock_offset int DEFAULT 0 ) RETURNS boolean AS $EOFCODE$
+CREATE FUNCTION totp.verify ( secret text, check_totp text, period int DEFAULT 30, digits int DEFAULT 6, time_from timestamptz DEFAULT now(), hash text DEFAULT 'sha1', encoding text DEFAULT 'base32', clock_offset int DEFAULT 0 ) RETURNS boolean AS $EOFCODE$
   SELECT totp.generate (
     secret,
     period,
@@ -330,9 +116,24 @@ CREATE FUNCTION totp.url ( email text, totp_secret text, totp_interval int, totp
     concat('otpauth://totp/', totp.urlencode (email), '?secret=', totp.urlencode (totp_secret), '&period=', totp.urlencode (totp_interval::text), '&issuer=', totp.urlencode (totp_issuer));
 $EOFCODE$ LANGUAGE sql STRICT IMMUTABLE;
 
-CREATE FUNCTION totp.random_base32 ( _length int DEFAULT 16 ) RETURNS text LANGUAGE sql AS $EOFCODE$
+CREATE FUNCTION totp.random_base32 ( _length int DEFAULT 20 ) RETURNS text LANGUAGE sql AS $EOFCODE$
   SELECT
     string_agg(('{a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,2,3,4,5,6,7}'::text[])[ceil(random() * 32)], '')
   FROM
     generate_series(1, _length);
 $EOFCODE$;
+
+CREATE FUNCTION totp.generate_secret ( hash text DEFAULT 'sha1' ) RETURNS bytea AS $EOFCODE$
+BEGIN
+    -- See https://tools.ietf.org/html/rfc4868#section-2.1.2
+    -- The optimal key length for HMAC is the block size of the algorithm
+    CASE
+          WHEN hash = 'sha1'   THEN RETURN totp.random_base32(20); -- = 160 bits
+          WHEN hash = 'sha256' THEN RETURN totp.random_base32(32); -- = 256 bits
+          WHEN hash = 'sha512' THEN RETURN totp.random_base32(64); -- = 512 bits
+          ELSE
+            RAISE EXCEPTION 'Unsupported hash algorithm for OTP (see RFC6238/4226).';
+            RETURN NULL;
+    END CASE;
+END;
+$EOFCODE$ LANGUAGE plpgsql VOLATILE;
