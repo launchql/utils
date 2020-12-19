eae9b156b36a9d6a9da842ea90ed4308b17a6efc
Dan Lynch <pyramation@gmail.com>
2020-12-18 19:37:16 -0700

    uuid

--- packages/status/revert/schemas/status_public/tables/user_task_achievement/triggers/achievement_triggers.sql
+++
@@ -1,8 +0,0 @@
--- Revert schemas/status_public/tables/user_task_achievement/triggers/achievement_triggers from pg
-
-BEGIN;
-
-DROP TRIGGER achievement_triggers ON status_public.user_task_achievement;
-
-
-COMMIT;
--- packages/status/verify/schemas/status_public/tables/user_task_achievement/triggers/achievement_triggers.sql
+++
@@ -1,8 +0,0 @@
--- Verify schemas/status_public/tables/user_task_achievement/triggers/achievement_triggers  on pg
-
-BEGIN;
-
-
-SELECT verify_trigger ('status_public.achievement_triggers');
-
-ROLLBACK;
